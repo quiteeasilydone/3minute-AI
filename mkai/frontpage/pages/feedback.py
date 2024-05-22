8dd1dcd569df80c6276a9a38b8f18eb5aaf7bcf4
quiteeasilydone <godricj1@gmail.com>
2024-05-22 22:20:19 +0900

    static page modified

--- mkai/frontpage/pages/feedback.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from streamlit_modal import Modal
-from st_pages import hide_pages
-
-st.set_page_config(
-    initial_sidebar_state="collapsed"
-)
-
-hide_pages(["main_page", "feedback", "interview", "upload_portfolio", "information"])

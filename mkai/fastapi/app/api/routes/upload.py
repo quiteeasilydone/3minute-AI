311d3275b57fe2a21ed3c89572ad2b7af4f1fd3e
hci <godricj1@gmail.com>
2024-06-26 14:37:23 +0900

    openai key

--- mkai/fastapi/app/api/routes/upload.py
+++ mkai/fastapi/app/api/routes/upload.py
@@ -8,7 +8,14 @@ router = APIRouter()
 memory_storage = {}
 
 @router.post("/api/upload")
-async def upload(file:UploadFile=File()):
+async def upload(file:UploadFile=File(), memory_storage = memory_storage):
+    memory_storage = {}
     content = await file.read()
     memory_storage[file.filename] = content
+    print(len(memory_storage.keys()))
     return JSONResponse(content={"filename": file.filename})
+
+@router.post("api/portfolio-analysis")
+async def asdk(filename):
+    content = memory_storage[filename].pop()
+    return
--- mkai/webpage/script.js
+++ mkai/webpage/script.js
@@ -1,8 +1,21 @@
 function redirectToInformation() {
     // console.log(JSON.stringify(window.location));
     const targetHost = '165.246.44.125';
-    const targetPort = 81;
+    const targetPort = 80;
     const targetPath = '/information';  // 리다이렉션할 경로
+
+    // const targetUrl = `http://${targetHost}:${targetPort}${targetPath}`;
+    const targetUrl = "http://" + targetHost + ":" + targetPort + targetPath + "/";
+    console.log(targetUrl);
+    window.location.href = targetUrl;
+    
+}
+
+function redirectToPortfolio() {
+    // console.log(JSON.stringify(window.location));
+    const targetHost = '165.246.44.125';
+    const targetPort = 80;
+    const targetPath = '/upload-portfolio';  // 리다이렉션할 경로
 
     // const targetUrl = `http://${targetHost}:${targetPort}${targetPath}`;
     const targetUrl = "http://" + targetHost + ":" + targetPort + targetPath + "/";
